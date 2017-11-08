41ed2368e0a982a9404a2d71201ef406b938abe6
Willie D. Pruitt Jr <w_pruitt@hotmail.com>
2017-11-08 08:42:17 -0600

    contentview page populates without any styling, except for content -- most likely due to being saved as html

--- app/Controllers/ContentViewController.js
+++ app/Controllers/ContentViewController.js
@@ -4,11 +4,19 @@
 // Controller handling DOM <-> DB interactions for ContentView page
 app.controller('ContentViewController', [
     '$scope',
+    '$routeParams',
     'FBDataFactory',
-    function($scope, FBDataFactory) {
+    function($scope, $routeParams, FBDataFactory) {
 
         // Retrieves contentspecified by contentId in url
-    
+        FBDataFactory.getContent($routeParams.contentId)
+        .then((content) => {
+            $scope.content - content;
+            console.log("content", $scope.content);
+        });
+
+        // Retrieves branches of content
+
     } 
 
 ]);

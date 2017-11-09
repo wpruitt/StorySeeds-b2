e0ded0dc2c5bb298763f0d040e0a1f80dbde3916
Willie D. Pruitt Jr <w_pruitt@hotmail.com>
2017-11-09 14:37:12 -0600

    grunt added w/ jshint, watch, sass

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        jshint: {
+            options: {
+                predef: ["document", "console", "$", "$scope", "firebase"],
+                esnext: true,
+                globalstrict: true,
+                globals: {"angular": true, "app": true, "alert": true}
+            },
+            files: ['app/**/*.js',
+                    'app/StorySeedsApp.js',
+                    '!app/components/**/*.js',
+                    '!app/bower_components/**'
+            ]
+        },
+        sass: {
+            dist: {
+                files: {
+                    'app/Css/main.css': 'app/scss/main.scss'
+                }
+            }
+        },
+        watch: {
+            javascripts: {
+                files: ['app/**/*.js',
+                        'app/StorySeedsApp.js',
+                        '!app/components/',
+                        '!app/bower_components/**'
+                ],
+                tasks: ['jshint']
+            },
+            sass: {
+                files: ['/app/Scss/**/*.scss'],
+                tasks: ['sass']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['jshint', 'sass', 'watch']);
+};

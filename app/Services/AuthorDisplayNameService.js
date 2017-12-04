81dbbf38af32b208632361ff31c75de1f11a33cf
Willie D. Pruitt Jr <w_pruitt@hotmail.com>
2017-12-04 12:46:03 -0600

    removed consolelogs from AuthorDisplayNameService

--- app/Services/AuthorDisplayNameService.js
+++ app/Services/AuthorDisplayNameService.js
@@ -7,14 +7,9 @@ app.service('AuthorDisplayNameService', [
         this.getDisplayNames = function() {
             FBDataFactory.getAllUsers()
             .then((userObjs) => {
-                console.log('userObjs', userObjs);
                 for(var key in userObjs) {
-                    console.log(key);
                     this.displayNames[userObjs[key].uid] = userObjs[key].displayName;
-                    console.log('displayNames', this.displayNames);
                 }
-                console.log('displayNames', this.displayNames);
-                return userObjs;
             });
         };
     }

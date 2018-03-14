95ae4f8e6919caf06b601a1c06eb2c7e0679bb09
Willie D. Pruitt Jr <w_pruitt@hotmail.com>
2018-03-14 18:38:28 -0500

    authorDisplayName directive handling anon appropriately"

--- app/Directives/AuthorDisplayNameDirective.js
+++ app/Directives/AuthorDisplayNameDirective.js
@@ -5,13 +5,19 @@
 app.directive('authorDisplayName', [
     function() {
         return {
-            template: `<h3>Author: {{author}}</h3>`,
+            template: `<a ng-href="#!/profile/{{uid}}"><h3>Author: {{author}}</h3></a>`,
             scope: {
                 uid: '=value'
             },
             link: function(scope, element, attrs) {
-                scope.displayNames = scope.$parent.displayNames;     
-                scope.author = scope.displayNames[scope.uid];
+                scope.displayNames = scope.$parent.displayNames; 
+                if(scope.uid){
+                    scope.author = scope.displayNames[scope.uid];
+                    console.log('scope.uid exist', scope.uid);
+                }else{
+                    scope.author = "anon";
+                    console.log('scope.uid does not exist', scope.uid);
+                }  
             }
         };
 }]);
